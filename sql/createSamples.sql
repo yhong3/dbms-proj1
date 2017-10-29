061878bca8c1b44dc7dccaad9772d07fb680377f
yhong3 <yhong3@ncsu.edu>
2017-10-29 00:04:38 -0400

    need to seperate drop and create

--- sql/createSamples.sql
+++ sql/createSamples.sql
@@ -1,23 +1,3 @@
---------------------------------------------------------
---  DROP ALL EXISTING TABLES
---------------------------------------------------------
-
-BEGIN
-
-    FOR c IN (SELECT table_name FROM user_tables) LOOP
-        EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE CONSTRAINTS');
-    END LOOP;
-
-    FOR s IN (SELECT sequence_name FROM user_sequences) LOOP
-        EXECUTE IMMEDIATE ('DROP SEQUENCE ' || s.sequence_name);
-    END LOOP;
-
-END;
-
---------------------------------------------------------
---  CREATE TABLES
---------------------------------------------------------
-
 CREATE TABLE COURSE(
 course_id VARCHAR(50),
 course_name VARCHAR(50) NOT NULL,
@@ -127,40 +107,13 @@ role INT,
 password VARCHAR(64)
 );
 
---------------------------------------------------------
---  INSERT SAMPLE DATA
---------------------------------------------------------
-
 Insert into COURSE (COURSE_ID,COURSE_NAME,COURSE_START,COURSE_END) values ('CSC440','Database Systems',to_date('27-AUG-17','DD-MON-RR'),to_date('12-DEC-17','DD-MON-RR'));
 Insert into COURSE (COURSE_ID,COURSE_NAME,COURSE_START,COURSE_END) values ('CSC540','Database Systems',to_date('25-AUG-17','DD-MON-RR'),to_date('10-DEC-17','DD-MON-RR'));
 Insert into COURSE (COURSE_ID,COURSE_NAME,COURSE_START,COURSE_END) values ('CSC541','Advanced Data Structures',to_date('25-AUG-17','DD-MON-RR'),to_date('06-DEC-17','DD-MON-RR'));
 
-
-Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC540','aneela',2);
-Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC541','mjones',2);
-Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC440','rchirkova',1);
-Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC540','kogan',1);
-Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC541','chealey',1);
-
-
-Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC440','tregan');
-Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC540','jmick');
-Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC440','mfisher');
-Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC440','jander');
-Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC541','jharla');
-Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC540','jmoyer');
-
-Insert into EXERCISE (EXERCISE_ID,EXERCISE_NAME,EXERCISE_START,EXERCISE_END,RETRIES_ALLOWED,NUM_OF_QUESTIONS,SCORING_POLICY_ID,CORRECT_ANSWER_POINTS,INCORRECT_ANSWER_PENALTY,DIFFICULTY_LEVEL_MIN,DIFFICULTY_LEVEL_MAX) values (1,'Homework 1 for CSC540',to_date('12-AUG-17','DD-MON-RR'),to_date('19-SEP-17','DD-MON-RR'),2,3,1,3,1,1,3);
-Insert into EXERCISE (EXERCISE_ID,EXERCISE_NAME,EXERCISE_START,EXERCISE_END,RETRIES_ALLOWED,NUM_OF_QUESTIONS,SCORING_POLICY_ID,CORRECT_ANSWER_POINTS,INCORRECT_ANSWER_PENALTY,DIFFICULTY_LEVEL_MIN,DIFFICULTY_LEVEL_MAX) values (2,'Homework 2 for CSC540 Adaptive Homework',to_date('21-SEP-17','DD-MON-RR'),to_date('10-OCT-17','DD-MON-RR'),1,3,3,4,1,3,5);
-Insert into EXERCISE (EXERCISE_ID,EXERCISE_NAME,EXERCISE_START,EXERCISE_END,RETRIES_ALLOWED,NUM_OF_QUESTIONS,SCORING_POLICY_ID,CORRECT_ANSWER_POINTS,INCORRECT_ANSWER_PENALTY,DIFFICULTY_LEVEL_MIN,DIFFICULTY_LEVEL_MAX) values (3,'Homework 3 for CSC540',to_date('12-OCT-17','DD-MON-RR'),to_date('30-OCT-17','DD-MON-RR'),-1,3,3,4,0,3,5);
-
 Insert into PROFESSOR (USER_ID,PROFESSOR_NAME) values ('kogan','Kemafor Ogan');
 Insert into PROFESSOR (USER_ID,PROFESSOR_NAME) values ('rchirkova','Rada Chirkova');
 Insert into PROFESSOR (USER_ID,PROFESSOR_NAME) values ('chealey','Christipher Healey');
-
-Insert into SCORING_POLICY (SCORING_POLICY_ID,SCORING_POLICY_TYPE) values (1,'Latest attempt');
-Insert into SCORING_POLICY (SCORING_POLICY_ID,SCORING_POLICY_TYPE) values (2,'Maximum score');
-Insert into SCORING_POLICY (SCORING_POLICY_ID,SCORING_POLICY_TYPE) values (3,'Average score');
 
 Insert into STUDENT (USER_ID,STUDENT_NAME,YEAR_ENROLLED,TYPE) values ('tregan','Tom Regan',2016,'Undergraduate');
 Insert into STUDENT (USER_ID,STUDENT_NAME,YEAR_ENROLLED,TYPE) values ('jmick','Jenelle Mick',2015,'Grad');
@@ -170,7 +123,6 @@ Insert into STUDENT (USER_ID,STUDENT_NAME,YEAR_ENROLLED,TYPE) values ('jharla','
 Insert into STUDENT (USER_ID,STUDENT_NAME,YEAR_ENROLLED,TYPE) values ('aneela','Aishwarya Neelakantan',2013,'Grad');
 Insert into STUDENT (USER_ID,STUDENT_NAME,YEAR_ENROLLED,TYPE) values ('mjones','Mary Jones',2015,'Grad');
 Insert into STUDENT (USER_ID,STUDENT_NAME,YEAR_ENROLLED,TYPE) values ('jmoyer','James Moyer',2013,'Grad');
-
 
 Insert into TOPIC (TOPIC_ID,TOPIC_NAME,COURSE_ID) values (1,'ER Model','CSC540');
 Insert into TOPIC (TOPIC_ID,TOPIC_NAME,COURSE_ID) values (2,'SQL','CSC540');
@@ -183,6 +135,27 @@ Insert into TOPIC (TOPIC_ID,TOPIC_NAME,COURSE_ID) values (8,'Sequential File Org
 Insert into TOPIC (TOPIC_ID,TOPIC_NAME,COURSE_ID) values (9,'BinarySearch','CSC540');
 Insert into TOPIC (TOPIC_ID,TOPIC_NAME,COURSE_ID) values (10,'Interpolation Search','CSC540');
 
+Insert into SCORING_POLICY (SCORING_POLICY_ID,SCORING_POLICY_TYPE) values (1,'Latest attempt');
+Insert into SCORING_POLICY (SCORING_POLICY_ID,SCORING_POLICY_TYPE) values (2,'Maximum score');
+Insert into SCORING_POLICY (SCORING_POLICY_ID,SCORING_POLICY_TYPE) values (3,'Average score');
+
+Insert into EXERCISE (EXERCISE_ID,EXERCISE_NAME,EXERCISE_START,EXERCISE_END,RETRIES_ALLOWED,NUM_OF_QUESTIONS,SCORING_POLICY_ID,CORRECT_ANSWER_POINTS,INCORRECT_ANSWER_PENALTY,DIFFICULTY_LEVEL_MIN,DIFFICULTY_LEVEL_MAX) values (1,'Homework 1 for CSC540',to_date('12-AUG-17','DD-MON-RR'),to_date('19-SEP-17','DD-MON-RR'),2,3,1,3,1,1,3);
+Insert into EXERCISE (EXERCISE_ID,EXERCISE_NAME,EXERCISE_START,EXERCISE_END,RETRIES_ALLOWED,NUM_OF_QUESTIONS,SCORING_POLICY_ID,CORRECT_ANSWER_POINTS,INCORRECT_ANSWER_PENALTY,DIFFICULTY_LEVEL_MIN,DIFFICULTY_LEVEL_MAX) values (2,'Homework 2 for CSC540 Adaptive Homework',to_date('21-SEP-17','DD-MON-RR'),to_date('10-OCT-17','DD-MON-RR'),1,3,3,4,1,3,5);
+Insert into EXERCISE (EXERCISE_ID,EXERCISE_NAME,EXERCISE_START,EXERCISE_END,RETRIES_ALLOWED,NUM_OF_QUESTIONS,SCORING_POLICY_ID,CORRECT_ANSWER_POINTS,INCORRECT_ANSWER_PENALTY,DIFFICULTY_LEVEL_MIN,DIFFICULTY_LEVEL_MAX) values (3,'Homework 3 for CSC540',to_date('12-OCT-17','DD-MON-RR'),to_date('30-OCT-17','DD-MON-RR'),-1,3,3,4,0,3,5);
+
+
+Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC540','aneela',2);
+Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC541','mjones',2);
+Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC440','rchirkova',1);
+Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC540','kogan',1);
+Insert into COURSE_STAFF (COURSE_ID,USER_ID,ROLE) values ('CSC541','chealey',1);
+
+Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC440','tregan');
+Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC540','jmick');
+Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC440','mfisher');
+Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC440','jander');
+Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC541','jharla');
+Insert into COURSE_STUDENT (COURSE_ID,USER_ID) values ('CSC540','jmoyer');
 
 Insert into USERID_PASSWORD (USER_ID,ROLE,PASSWORD) values ('tregan',3,'tregan');
 Insert into USERID_PASSWORD (USER_ID,ROLE,PASSWORD) values ('jmick',3,'jmick');
