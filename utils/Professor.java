d3f8aabecad1ffa1930c93bc706914aa15450c1e
cecilia <yxi2@ncsu.edu>
2017-11-05 01:32:00 -0400

    Update Professor.java

--- utils/Professor.java
+++ utils/Professor.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import utils.Course;
+import utils.Topics;
 
 public class Professor {
     
@@ -25,43 +26,43 @@ public class Professor {
     	Boolean returnToRoot = true;
     	
     	while (selection != "5" && instructorReturn && returnToRoot) {
-    		
-    		Menu.instructorMainMenu();
-            selection = scanner.nextLine();
-            
-            switch (selection) {  
-            case "0": 
-            	instructorReturn = false;
-            	break;
-   			   
-            // View Profile
-            case "1":
-            	instructorProfile(connection, uid);
-                break;
-            // View/Add Course 
-            case "2":
-            	returnToRoot = instructorViewCourseProfile(connection, uid);
-            	break;
-            // Enroll/Drop a student	
-            case "3":
-            	returnToRoot = instructorEnrollStudent(connection, uid);
-            	break;
-            // Search/Add questions to Question Bank 
-            case "4":
-            	returnToRoot = instructorSearchAddQuestion(connection, uid);
-	            break;
-	        // Logout     	
-            case "5":
-            	Menu.returnLoginMessage();
-            	returnToRoot = false;
-            	return returnToRoot;
-            	
-            default:
-            	Menu.warningMessage();
-            	break;
+		
+		Menu.instructorMainMenu();
+		selection = scanner.nextLine();
+		
+		switch (selection) {  
+		    case "0": 
+			instructorReturn = false;
+			break;
+
+		    // View Profile
+		    case "1":
+			instructorProfile(connection, uid);
+			break;
+		    // View/Add Course 
+		    case "2":
+			returnToRoot = instructorViewCourseProfile(connection, uid);
+			break;
+		    // Enroll/Drop a student	
+		    case "3":
+			returnToRoot = instructorEnrollStudent(connection, uid);
+			break;
+		    // Search/Add questions to Question Bank 
+		    case "4":
+			returnToRoot = instructorSearchAddQuestion(connection, uid);
+			    break;
+			// Logout     	
+		    case "5":
+			Menu.returnLoginMessage();
+			returnToRoot = false;
+			return returnToRoot;
+
+		    default:
+			Menu.warningMessage();
+			break;
             }//end switch
     	}//end while
-		return returnToRoot;
+	return returnToRoot;
     }
     
    // View/Add Course:  Instructor view/add a course in the COURSE table
@@ -144,7 +145,6 @@ public class Professor {
 	   return returnToRoot;
    }
    
-   // TODO: Merge
    // Search/Add questions to Question Bank:  Instructor search/add questions to question bank
    static Boolean instructorSearchAddQuestion(Connection connection, String uid) throws Throwable {
 	   	Scanner scanner = new Scanner(System.in);
@@ -275,49 +275,193 @@ public class Professor {
     }
     
     // View/Add Course:  Instructor add a new course to COURSE table
-    private static void instructorAddCourse(Connection connection, String uid) throws Throwable
+    static boolean instructorAddCourse(Connection connection, String uid) throws Throwable
     {   
     	Scanner scanner = new Scanner(System.in);
     	Menu.addCourseMessage(); 
+    	Boolean returnToRoot = true;
     	
     	System.out.println("Add a course_id: \n");
     	String cid = scanner.nextLine();
     	
-    	// Check if the course has already exist
+    	String addCourse_selection = "1"; // selection for Add Course
+	Boolean addCourseReturn = true;
+
+	while (addCourse_selection != "4" && addCourseReturn && returnToRoot) {
+		   Menu.addCourseMenu();
+		   addCourse_selection = scanner.nextLine();
+
+		   switch (addCourse_selection) {
+		   case "0":
+			   addCourseReturn = false;
+			   break;
+		   // Add course basic infos (Insert into the table of COURSE)
+		   case "1":
+			   // Check if the course has already exist
+			if (Course.checkCourseExist(connection, cid)) { Menu.addCourseFailureMessage(); }
+
+			else {
+
+				System.out.println("Enter the Basic Infos of this New Course: \n");
+				addCourseBasicInfo(connection, cid);
+				Menu.addCourseSuccessMessage(cid);
+			}
+			   break;
+
+		   // Add course instructor and TAs (Insert into the table of COURSE_STAFF)
+		   case "2":
+			   System.out.println("Add an instructor or TA for this New Course: \n");
+			   returnToRoot = addStaffForCourse(connection, cid);
+			   break;
+
+		   // Add course topics (Insert into the table of TOPIC)
+		   case "3":
+			   Topics.addTopic(connection, scanner, cid);
+			   break;
+
+		   case "4":
+			   Menu.returnLoginMessage();
+			   returnToRoot = false;
+			   return returnToRoot;
+
+		   default:
+			   Menu.warningMessage();
+			   break;
+		   }//end switch
+	   }//end while 
+	return returnToRoot;
+    }
+	
+    static void addCourseBasicInfo(Connection connection, String cid) throws Throwable {
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	
     	if (Course.checkCourseExist(connection, cid)) { Menu.addCourseFailureMessage(); }
     	
     	else {
     		
-    		System.out.println("Add a course_name: \n");
-        	String cname = scanner.nextLine();
-        	
-        	System.out.println("Add a course_start: \n");
-        	String cstart = scanner.nextLine();
-        	
-        	System.out.println("Add a course_end: \n");
-        	String cend = scanner.nextLine();
-        	
-        	SimpleDateFormat format= new SimpleDateFormat("yyyy/mm/dd"); //  hh:mi:ss AM
-            java.util.Date dstart= format.parse(cstart);
-            java.util.Date dend= format.parse(cend);
-        	
-            // Converting java.util.Date value to java.sql.Date class obj
-            long constart = dstart.getTime();
-            java.sql.Date qstart= new java.sql.Date(constart);
-            long conend = dend.getTime();
-            java.sql.Date qend= new java.sql.Date(conend);
-        	
-        	PreparedStatement preparedStatement = connection.prepareStatement(SqlQueries.SQL_INSERTCOURSE);
-        	preparedStatement.setString(1, cid);
-        	preparedStatement.setString(2, cname);
-        	preparedStatement.setDate(3, qstart);
-        	preparedStatement.setDate(4, qend);
-        	preparedStatement.execute();
-        	
-        	Menu.addCourseSuccessMessage(cid);
+    		// Add course basic infos (Insert into the table of COURSE)
+			System.out.println("Add a course_name: \n");
+	    	String cname = scanner.nextLine();
+	    	
+	    	System.out.println("Add a course_start: \n");
+	    	String cstart = scanner.nextLine();
+	    	
+	    	System.out.println("Add a course_end: \n");
+	    	String cend = scanner.nextLine();
+	    	
+	    	System.out.println("Add a course_level: \n");
+	    	String clevel = scanner.nextLine();
+	    	
+	    	SimpleDateFormat format= new SimpleDateFormat("yyyy/mm/dd"); //  hh:mi:ss AM
+	        java.util.Date dstart= format.parse(cstart);
+	        java.util.Date dend= format.parse(cend);
+	    	
+	        // Converting java.util.Date value to java.sql.Date class obj
+	        long constart = dstart.getTime();
+	        java.sql.Date qstart= new java.sql.Date(constart);
+	        long conend = dend.getTime();
+	        java.sql.Date qend= new java.sql.Date(conend);
+    	
+	    	PreparedStatement preparedStatement = connection.prepareStatement(SqlQueries.SQL_INSERTCOURSE);
+	    	preparedStatement.setString(1, cid);
+	    	preparedStatement.setString(2, cname);
+	    	preparedStatement.setDate(3, qstart);
+	    	preparedStatement.setDate(4, qend);
+	    	preparedStatement.setString(5, clevel);
+	    	preparedStatement.execute();
     	}
     }
-    
+
+// Add a staff to the new added course 
+	static boolean addStaffForCourse(Connection connection, String cid) throws Throwable {
+    	
+    	Scanner scanner = new Scanner(System.in);    	
+    	String selection = "1";
+    	Boolean addStaffView = true;
+    	Boolean returnToRoot = true;
+    	
+    	while (selection != "7" && addStaffView && returnToRoot) {
+    		Menu.addInstructorTAMenu();
+    		
+    		selection = scanner.nextLine();
+    		
+    		switch (selection) {
+    		case "0": 
+    			addStaffView = false;
+            	break;
+    		
+    		case "1":
+    			System.out.println("\n**Please enter the user_id of instructor.**");
+    			// The id of instructor being assigned
+    			String iid = scanner.nextLine();
+    			
+    			// check if there has been instructor assigned to the course
+    			// If already assigned
+    			if (!checkInstructorAssigned(connection, cid) && checkInstructorExist(connection, iid)) {
+    				// execute the query to insert instructor into COURSE_STAFF table
+    				PreparedStatement preparedStatement = connection.prepareStatement(SqlQueries.SQL_ASSIGNINSTRUCTOR);
+				preparedStatement.setString(1, cid);
+				preparedStatement.setString(2, iid);
+				preparedStatement.execute();
+
+				System.out.println("\n**Instructor " + iid + " has been assigned for the course " + cid + ".**");
+    			}
+    			else if (checkInstructorAssigned(connection, cid) && checkInstructorExist(connection, iid))
+    			{
+    				System.out.println("\n**This course has already been assigned to another instructor.**");
+    			}
+    			else {
+    				System.out.println("\n**The assigned instructor does not exist in the system.**");
+    			}
+    			break;
+    			
+    		case "2":
+    			Course.instructorEnrollTA(connection, cid);
+    			break;
+    			
+    		case "3":
+			Menu.returnLoginMessage();
+			returnToRoot = false;
+			return returnToRoot;
+
+		default:
+			Menu.warningMessage();
+			break;
+    		}//end switch
+    	}//end while
+    	return returnToRoot;
+    }
+	
+// Check if a course has been assigned an instructor
+    static boolean checkInstructorAssigned(Connection connection, String cid) throws Throwable
+    {
+    	Boolean inassign = false; 
+    	PreparedStatement preparedStatement_inassign = connection.prepareStatement(SqlQueries.SQL_CHECKCOURSEINS);
+    	preparedStatement_inassign.setString(1, cid);
+    	ResultSet rs_inassign = preparedStatement_inassign.executeQuery();
+    	if (rs_inassign.next()) {
+    		inassign = true;
+    	}
+	return inassign;
+    }
+
+// Check if instructor exist in the system
+    static boolean checkInstructorExist (Connection connection, String uid) throws SQLException
+    {
+    	Boolean iexist = false; 
+    	PreparedStatement preparedStatement_senroll = connection.prepareStatement(SqlQueries.SQL_PROFPROFILE);
+    	preparedStatement_senroll.setString(1, uid);
+    	ResultSet rs_senroll = preparedStatement_senroll.executeQuery();
+    	if (rs_senroll.next()) {
+    		iexist = true;
+    	}
+    	
+	return iexist;
+    }
+	
+	
+	
     // Instructor Main Menu --> Enroll/Drop a student:  Enroll student to course
     static void enrollStudent(Connection connection, String uid) throws Throwable
     {
@@ -415,7 +559,7 @@ public class Professor {
     	}
     }
     
-	// View/Add Course -- > Enroll/Drop a student:  Drop student from a course
+// View/Add Course -- > Enroll/Drop a student:  Drop student from a course
     static void dropStudentCourse(Connection connection, String cid) throws Throwable
     {
     	Scanner scanner = new Scanner(System.in);
